5be301a71e1af905e54f555fe38caa0e4c9c82c1
Khrystyna Sen <anollina@ukr.net>
2025-03-30 22:56:00 +0300

    solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,15 +1,17 @@
+from django.db import transaction
 from django.db.models import QuerySet
-
-from db.models import MovieSession
+from db.models import MovieSession, Ticket, Movie, CinemaHall
 
 
 def create_movie_session(
     movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        movie_id=movie_id,
-        cinema_hall_id=cinema_hall_id,
+        movie=movie,
+        cinema_hall=cinema_hall,
     )
 
 
@@ -24,6 +26,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
+@transaction.atomic
 def update_movie_session(
     session_id: int,
     show_time: str = None,
@@ -34,11 +37,18 @@ def update_movie_session(
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
-        movie_session.movie_id = movie_id
+        movie_session.movie = Movie.objects.get(id=movie_id)
     if cinema_hall_id:
-        movie_session.cinema_hall_id = cinema_hall_id
+        movie_session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    tickets = Ticket.objects.filter(movie_session__id=movie_session_id)
+    taken_seats = [{"row": ticket.row, "seat": ticket.seat}
+                   for ticket in tickets]
+    return taken_seats
---
+++ services/order.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from django.db.models import QuerySet
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.utils.dateparse import parse_datetime
+from db.models import Order, Ticket, MovieSession
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: Optional[str] = None
+) -> Order:
+    user = get_user_model().objects.get(username=username)
+
+    if date:
+        created_at = parse_datetime(date)
+    else:
+        created_at = timezone.now()
+
+    order = Order.objects.create(user=user, created_at=created_at)
+
+    for ticket_data in tickets:
+        movie_session = (
+            MovieSession.objects.get(id=ticket_data["movie_session"]))
+        ticket = Ticket(
+            order=order,
+            movie_session=movie_session,
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+        )
+        ticket.full_clean()
+        ticket.save()
+
+    return order
+
+
+def get_orders(username: Optional[str] = None) -> QuerySet:
+    orders = Order.objects.select_related("user")
+    if username:
+        orders = orders.filter(user__username=username)
+    return orders
